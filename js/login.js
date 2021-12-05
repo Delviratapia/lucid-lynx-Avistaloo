603d4f1a9b1e8e81ac03c7c1968e51876c7d8231
Dominique Elvira <delviratapia@gmail.com>
2021-12-04 19:40:44 -0500

    Merged feature/login into feature/navbar

--- js/login.js
+++ js/login.js
@@ -1,31 +1,20 @@
-document.querySelector(".modal-button").addEventListener("click", function() {
-    let target = this.getAttribute("data-target")
-    document.querySelector("html").classList.add("is-clipped")
-    document.querySelector(target).classList.add("is-active")
-})
+let qs = document.querySelector.bind(document)
+let qsa = document.querySelectorAll.bind(document)
+let buttons = qsa(".modal-button")
 
-document.querySelector(".modal-close").addEventListener("click", function() {
-    document.querySelector("html").classList.remove("is-clipped")
-    this.parentNode.classList.remove("is-active")
-})
+for (let button of buttons) {
+    button.addEventListener("click", function () {
+        let target = this.getAttribute("data-target")
+        console.log(target)
+        qs("html").classList.add("is-clipped")
+        qs(target).classList.add("is-active")
+    })
+}
 
-// login button
-document.querySelector("#one").addEventListener("click", function() { showTab(this)})
-// register button
-document.querySelector("#two").addEventListener("click", function() {showTab(this)});
-
-const showTab = (e) => {
-    let selectType = e.getAttribute("data-select")
-
-    if (selectType == 'one') {
-        document.querySelector("#tabs1").classList.remove("is-hidden")
-        document.querySelector("#tabs2").classList.add("is-hidden")
-        document.querySelector("#activeIdOne").classList.add("is-active")
-        document.querySelector("#activeIdTwo").classList.remove("is-active")
-    } else if (selectType == 'two') {
-        document.querySelector("#tabs1").classList.add("is-hidden")
-        document.querySelector("#tabs2").classList.remove("is-hidden")
-        document.querySelector("#activeIdOne").classList.remove("is-active")
-        document.querySelector("#activeIdTwo").classList.add("is-active")
-    }
+let exitButtons = qsa(".modal-close")
+for (let button of exitButtons) {
+    button.addEventListener("click", function () {
+        qs("html").classList.remove("is-clipped")
+        this.parentNode.classList.remove("is-active")
+    })
 }
