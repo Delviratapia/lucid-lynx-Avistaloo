5e6359c7700f65858289ac1d94655b9a08cbdb68
Dominique Elvira <delviratapia@gmail.com>
2021-12-12 20:31:10 -0500

    added carousel and images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#335963',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+
+  ],
 };
