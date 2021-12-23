ee97727c6bb43c7553dfb5a75e194e846ffa70e6
Manuel Navarro <manuelnhc@gmail.com>
2021-12-23 14:39:05 +0000

    Landing Finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     extend: {
       colors:{
         'primary-color': '#335963',
+        'color-domi': '#EEE',
+      },
+      screens: {
+        navxs: { min: "0px", max: "336px" },
+
+        navsm: { min: "337px", max: "767px" },
+
+        navmd: { min: "768px", max: "1102px" },
+
+        navlg: { min: "1103px", max: "5000px" },
       },
       backgroundImage:{
         'header-background': "url('/src/assets/styles/images/background-header.jpg')",
