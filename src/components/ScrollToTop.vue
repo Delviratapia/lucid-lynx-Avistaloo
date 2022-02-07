c67f2f730f3d38197ad3bd17c1da36e7f3d3e245
Carlos Da Silva <carloshdezdasilva@gmail.com>
2022-02-07 20:29:28 +0000

    First commit, trying to make a Scroll to top button

---
+++ src/components/ScrollToTop.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button v-on:click="scrollToTop" type="button">To The Top</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: {
+    name: "ScrollToTop",
+  },
+  methods: {
+    scrollToTop() {
+      window.scrollTo(0, 0);
+    },
+  }
+}
+</script>
+
+<style>
+</style>
