c1340923ebd568e16dd52e52c148ff3bd2ff9c60
Carlos Da Silva <carloshdezdasilva@gmail.com>
2022-02-08 18:09:48 +0000

    Scroll to top button's text switched to symbol 2: electric boogaloo

---
+++ src/components/ScrollToTop.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <button
+      id="toTopButton"
+      v-on:click="scrollToTop"
+      type="button"
+      alt="Botón de vuelta al inicio"
+    >🠕</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    scrollToTop() {
+      window.scrollTo(0, 0);
+    },
+  }
+}
+</script>
+
+<style>
+#toTopButton {
+  padding: 2px 7px 2px 7px;
+  border: solid 1px white;
+  border-radius: 5px;
+
+  background: #335963;
+  color: white;
+
+  position: fixed;
+  right: 10px;
+  bottom: 10px;
+  transition: 0.3s;
+  z-index: 2000;
+
+  font-weight: bolder;
+  font-size: 50px;
+}
+
+#toTopButton:hover {
+  transform: scale(1.1);
+}
+</style>
